729808e5f4037bae080c6146e8d49cd2216cb65f
deekshithn2004 <157571864+deekshithn2004@users.noreply.github.com>
2026-07-04 09:00:48 +0530

    Add: longest-common-prefix - submission-0

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-0.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+    Arrays.sort(strs);
+    String frst=strs[0];
+    String last=strs[strs.length-1];
+    int i=0;
+    while(i<frst.length()&&i<last.length()&&frst.charAt(i)==last.charAt(i)){
+        i++;
+    } 
+    return last.substring(0,i);
+    }
+}
